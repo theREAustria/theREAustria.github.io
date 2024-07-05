c284c5ce973a8438808aa7126b6c51c8f4882715
Renz Edwin <re.renzedwinaustria@gmail.com>
2024-07-05 20:46:32 +0800

    initial commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("hi1")
+spanHi.classList.toggle("hi2")
+
+spanHi.style.color = "red"
