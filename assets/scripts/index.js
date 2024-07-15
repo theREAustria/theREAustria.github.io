e3fbee78f6b56a72c90742c131c8b87bdee00b08
Renz Edwin Austria <re.renzedwinaustria@gmail.com>
2024-07-16 04:15:44 +0800

    update assets

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,9 @@
-const div = document.querySelector("div")
-const spanHi = document.querySelector("#hi")
-const spanBye = document.querySelector("#bye")
+// const div = document.querySelector("div")
+// const spanHi = document.querySelector("#hi")
+// const spanBye = document.querySelector("#bye")
 
-spanHi.classList.add("new-class")
-spanHi.classList.remove("hi1")
-spanHi.classList.toggle("hi2")
+// spanHi.classList.add("new-class")
+// spanHi.classList.remove("hi1")
+// spanHi.classList.toggle("hi2")
 
-spanHi.style.color = "red"
+// spanHi.style.color = "red"
